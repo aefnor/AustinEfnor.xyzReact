ad4e8b9745427f36f61a317dc583fc68026bf06c
Austin Efnor <aefnor@yahoo.com>
2019-01-21 10:46:35 -0700

    test

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Link from 'react-router-dom/Link'
 import "../styles/Home.css"
 import Background from "../assets/Background.png"
 import Button from '@material-ui/core/Button';
@@ -6,6 +7,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
+import Linkify from 'react-linkify'
 
 class Home extends Component {
     constructor(props) {
@@ -19,47 +21,30 @@ class Home extends Component {
             [name]: event.target.value,
         });
     };
+     handleClick(e) {
+        e.preventDefault();
+        console.log('The link was clicked.');
+    }
     
     //https://blog.visme.co/website-color-schemes/
     render(){
         let homeContainerHeight = {
             height: `${this.props.height}px`
           }
+        let url = "www.intern.supply/"
         console.log("Props: ", this.props)
         
         return(
             <div className="homeContainer" style={homeContainerHeight}>
                 <div className="homeHeaderContainer">
-                    {/* <img src={Background} className="backgroundImage"/> */}
                     <h3 className="homeHeaderText">Austin Efnor</h3>
                 </div>
                 <div className="homeBodyContainer">
                     <p className="description">
-                        I Co-Created Intern Supply, a website which sent over 400,000 applicants to internship applications in the Fall 2016 recruiting season.
+                        I Co-Created , a website which sent over 400,000 applicants to internship applications in the Fall 2016 recruiting season.
                         I have maintained an internship since my freshman year of college, totalling me to 3 years of work experience in a full production environment.
                     </p>
-                    <div className="emailContainer">
-                        {/* <TextField
-                            id="outlined-name"
-                            label="Name"
-                            className="test"
-                            // value={this.state.name}
-                            onChange={this.handleChange('name')}
-                            margin="normal"
-                            variant="outlined"
-                        />
-                        <TextField
-                            id="outlined-email-input"
-                            label="Email"
-                            // className={classes.textField}
-                            type="email"
-                            name="email"
-                            autoComplete="email"
-                            margin="normal"
-                            variant="outlined"
-                        /> */}
-                        {/* <img src={Background} className="backgroundImage" /> */}
-                    </div>
+                    <a href="https://www.google.com/">open</a>
                 </div>
             </div>
         )
